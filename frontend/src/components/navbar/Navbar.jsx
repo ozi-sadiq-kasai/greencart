d275f7b241b88337107383aa27b17d03dfed55d9
ozi <ozisadiqkasai@gmail.com>
2025-04-22 20:12:02 +0100

    navbar done

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,8 +1,14 @@
 import { assets } from "../../assets/images/assets";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import "./navbar.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+    console.log(isOpen);
+  };
   return (
     <nav className="navbar">
       <img src={assets.logo} alt="" />
@@ -28,8 +34,33 @@ const Navbar = () => {
           </Link>
           <div className="tab">0</div>
         </div>
-          <Link to="/login" className="login">Login</Link>
-          <img src={assets.menu_icon} alt="" className="menu"/>
+        <Link to="/login" className="login">
+          Login
+        </Link>
+        <img
+          src={assets.menu_icon}
+          alt=""
+          className="menu"
+          onClick={toggleMenu}
+        />
+        {isOpen && (
+          <div className="mobile-menu">
+            <ul className="nav-links-mobile">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/products">All Products</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+              <li className="login-mobile">
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </nav>
   );
